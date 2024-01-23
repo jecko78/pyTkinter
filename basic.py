b110e04f04fe9ae360972756c15d79bb4cea39bd
jecko78 <jecko78@gmail.com>
2024-01-23 11:06:26 +0700

    title,iconbitmap, geometry,resizeable,config

--- basic.py
+++ basic.py
@@ -3,4 +3,11 @@
 from tkinter import *
 
 root = Tk()
+root.title("ໂປແກມຂອງຂ້ອຍ")
+root.iconbitmap("icons/favicon.ico")
+root.geometry("800x600+200+0")
+root.resizable(1,1)
+root.config(bg="#FFD599")
+
+
 root.mainloop()
