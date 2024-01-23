ed07611958358e804403dc9b26063ce072dbdfde
jecko78 <jecko78@gmail.com>
2024-01-23 11:11:19 +0700

    Gold Windows

--- basic.py
+++ basic.py
@@ -5,9 +5,9 @@ from tkinter import *
 root = Tk()
 root.title("ໂປແກມຂອງຂ້ອຍ")
 root.iconbitmap("icons/favicon.ico")
-root.geometry("800x600+200+0")
-root.resizable(1,1)
-root.config(bg="#FFD599")
+root.geometry("640x480+300+50")
+root.resizable(0,0)
+root.config(bg="#FFD700")
 
 
 root.mainloop()
